4f4c43fe1b119a8d6f1ca1bcc3f6e5af0c4455a7
alex.szeto <alex.szeto.a@gmail.com>
2020-02-28 00:16:57 -0500

    Done.

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ class Formatter {
   }
 
   static sanitize(input){
-    return input.replace( /[^A-Za-z0-9 ']/g, "")
+    return input.replace( /[^\w\d\s\-']/g, "")
   }
 
   static titleize(input){
-
+    let banned = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    let split_arr = input.split(" ")
+    split_arr[0] = split_arr[0][0].toUpperCase() + split_arr[0].slice(1)
+    for(let i = 1; split_arr.length > i; i++){
+      if(banned.includes(split_arr[i])){continue}
+      split_arr[i] = split_arr[i][0].toUpperCase() + split_arr[i].slice(1)
+    }
+    return split_arr.join(" ")
   }
 }
